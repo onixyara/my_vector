ed44f246adc6d4d61c6f6eda062c2f0b52ebff71
onixyara <onixyarka@gmail.com>
2019-09-20 14:46:25 +0300

    template

--- vector.cpp
+++ vector.cpp
@@ -1,46 +1,54 @@
 #include "vector.hpp"
 
-Vector::Vector()
+template <class T>
+Vector<T>::Vector()
 {
 	m_capacity = MIN_CAPACITY;
 	m_size = 0;
-	arr = new int[m_capacity];
+	arr = new T[m_capacity];
 }
 
-Vector::~Vector()
+template <class T>
+Vector<T>::~Vector()
 {
 	delete arr;
 }
 
-void		Vector::push_back(int value)
+template <class T>
+void		Vector<T>::push_back(T value)
 {
 	if (m_size >= m_capacity)
 		resize();
 	*(arr + m_size++) = value;
 }
 
-size_t		Vector::size() const
+template <class T>
+size_t		Vector<T>::size() const
 {
 	return	m_size;
 }
 
-bool		Vector::empty() const
+template <class T>
+bool		Vector<T>::empty() const
 {
 	return m_size == 0;
 }
 
-void		Vector::move(size_t start)
+template <class T>
+void		Vector<T>::move(size_t start)
 {
 	for (size_t i = start; i < m_size; i++)
 		std::swap(arr[i], arr[i + 1]);	
 }
 
-void		Vector::clear()
+template <class T>
+void		Vector<T>::clear()
 {
 	m_size = 0;
 }
 
-void		Vector::erase(size_t pos)
+template <class T>
+void		Vector<T>::erase(size_t pos)
 {
 	if (pos > m_size)
 		throw std::out_of_range("erasing an element out of bounds");
@@ -48,10 +56,11 @@ void		Vector::erase(size_t pos)
 	m_size--;
 }
 
-void		Vector::resize()
+template <class T>
+void		Vector<T>::resize()
 {
 	size_t	capacity = m_capacity * CAP_FACTOR;
-	int		*tmp = new int[capacity];
+	T		*tmp = new T[capacity];
 	for (size_t i = 0; i < m_capacity; i++)
 		tmp[i] = arr[i];
 	delete arr;
--- vector.hpp
+++ vector.hpp
@@ -6,17 +6,18 @@
 #define MIN_CAPACITY 10
 #define	CAP_FACTOR	 2
 
+template <class T>
 class		Vector{
 	public:
 		Vector();
 		~Vector();
 
-		void	push_back(int value);
+		void	push_back(T value);
 		size_t	size() const;
 		void	erase(size_t pos);
 		void	clear();
 		bool	empty() const;
-		int		&operator[](size_t pos){
+		T		operator[](size_t pos){
 			if (pos > m_size)
 				throw	std::out_of_range("element out of bound");
 			else
@@ -25,7 +26,7 @@ class		Vector{
 	private:
 		size_t	m_size;
 		size_t	m_capacity;
-		int		*arr;
+		T*		arr;
 		void	resize();
 		void	move(size_t start);
 };
